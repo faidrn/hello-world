a1acd8997bc5c9646ae44aec85298304f9c56055
faidrn <m3talmaniaco@gmail.com>
2022-06-23 16:16:34 -0500

    Test 2 js file

--- js/minecraft-house.js
+++ js/minecraft-house.js
@@ -4,5 +4,5 @@ add code to repeat divs with classes fence1-front-brick1 and fence1-front-brick2
 
 // Crear nodo de tipo Element
 var miDiv = document.createElement("div");
-var capa = document.getElementById("capa");
+var capa = document.getElementById("fence1-front");
 capa.appendChild(miDiv);
