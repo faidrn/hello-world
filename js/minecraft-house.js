1e82acb38a87bdd36e3977f544d77140d2aba02d
Fredy Izquierdo <m3talmaniaco@gmail.com>
2022-06-24 13:05:47 -0500

    Erraser comments from superman's log and add divs to minecraft house with JavaScript

--- js/minecraft-house.js
+++ js/minecraft-house.js
@@ -2,7 +2,44 @@
 add code to repeat divs with classes fence1-front-brick1 and fence1-front-brick2
  */
 
-// Crear nodo de tipo Element
-var miDiv = document.createElement("div");
-var capa = document.getElementById("fence1-front");
-capa.appendChild(miDiv);
+let brick2 = document.getElementsByClassName("fence1-front-brick2")[0];
+
+let marginLeft = 2.3;
+
+for (let i = 0; i <= 20; i++){
+    // Crear nodo de tipo Element
+    let miDiv = document.createElement("div");
+
+    //Agregar atributos
+    miDiv.setAttribute("class", "fence1-front-brick1 fence1-front-brick1-js");
+    
+    //miDiv.setAttribute("margin-left", marginLeft + "vw");
+    miDiv.style.marginLeft = marginLeft + "vw";
+    marginLeft += 2.2;
+
+    //elegir ubicacion
+    let capa = document.getElementById("fence1-front-id");
+    //agregar
+    //capa.appendChild(miDiv);
+    capa.insertBefore(miDiv, brick2);
+}
+
+
+marginLeft = 2.3;
+
+for (let i = 0; i <= 20; i++){
+    // Crear nodo de tipo Element
+    let miDiv = document.createElement("div");
+
+    //Agregar atributos
+    miDiv.setAttribute("class", "fence1-front-brick2 fence1-front-brick2-js");
+    
+    miDiv.style.marginLeft = marginLeft + "vw";
+    marginLeft += 2.2;
+
+    //elegir ubicacion
+    let capa = document.getElementById("fence1-front-id");
+    //agregar
+    capa.appendChild(miDiv);
+    //capa.insertBefore(miDiv, brick2);
+}
